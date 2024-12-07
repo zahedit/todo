2e819d3807605ba37373291b69c5137cf99b8409
amir <redwolf.ir1993@gmail.com>
2024-12-07 20:54:30 +0330

    add the adding and showing items feature

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 import express from "express";
 import bodyParser from "body-parser";
 import connectDB from "./db.js";
+import Item from "./models/Item.js";
+
+//---------------------------------------------------
 
 const app = express();
 const port = 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 connectDB();
 
-app.get("/", async (req, res) => {
-    res.render("index.ejs", {});
+//---------------------------------------------------
+
+const item1 = new Item ({
+    name: "Playing AOE 4",
 });
+const item2 = new Item ({
+    name: "Reading my Favorite book",
+});
+const item3 = new Item ({
+    name: "Make one more todo item!",
+});
+const defaultItems = [item1, item2, item3];
+
+//---------------------------------------------------
+
+app.get("/", async (req, res) => {
+    const items = await Item.find();
+    if (items.length === 0) {
+        defaultItems.forEach(async item => {
+            item.save();
+            console.log(`Added: ${item.name}`);
+        });
+    }
+
+    res.render("index.ejs", {items});
+});
+
+app.post("/", (req, res) =>{
+    const newItem = new Item({
+        name: req.body["todo"]
+    });
+    newItem.save();
+    res.redirect("/");
+});
+
+//---------------------------------------------------
 
 app.listen(port, function() {
     console.log(`App is running on port ${port}`);
