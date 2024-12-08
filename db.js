547c67765552d38d245de0852db9bc58bd88d239
amir <redwolf.ir1993@gmail.com>
2024-12-08 11:55:22 +0330

    fix database connection issue

--- db.js
+++ db.js
@@ -5,11 +5,14 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected successfully!");
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1);
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // Exit process with failure
   }
 };
 
